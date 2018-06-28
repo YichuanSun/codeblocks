9a62e15a5518fef914302d9f6dc08d777fda56b8
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-06-29 00:00:12 +0800

    .

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,10 +1,38 @@
 #include <bits/stdc++.h>
+#define N 1005
 using namespace std;
 
-int main()  {
-    string str,strnew;
-    cin>>str;
-    strnew=str.substr(3,2);
-    cout<<strnew<<endl;
+int n,W,v[N],w[N],dp[N][N];
+
+void solve()    {
+    for (int i=0;i<n;i++)   {
+        for (int j=1;j<=W;j++)  {
+            if (j<w[i-1]) dp[i][j]=dp[i-1][j];
+            else dp[i][j]=max(dp[i-1][j],dp[i-1][j-w[i-1]]+v[i-1]);
+        }
+    }
+}
+/*
+void solve()    {
+    for (int i=0;i<n;i++)   {
+        for (int j=0;j<=W;j++)  {
+            if (j<w[i]) dp[i+1][j]=dp[i][j];
+            else dp[i+1][j]=max(dp[i][j],dp[i][j-w[i]]+v[i]);
+        }
+    }
+}
+*/
+int main () {
+    cin>>n;
+    for (int i=0;i<n;i++)   cin>>w[i];
+    for (int i=0;i<n;i++)   cin>>v[i];
+    cin>>W;
+    solve();
+    for (int i=0;i<=n;i++)  {
+        for (int j=0;j<=W;j++)
+            cout<<'\t'<<dp[i][j];
+        cout<<endl;
+    }
+    cout<<dp[n-1][W]<<endl;
     return 0;
 }
