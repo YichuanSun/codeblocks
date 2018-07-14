86c61fef47c6176039b3710276d4f1824b5e262a
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-07-14 09:51:46 +0800

    f

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,10 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+typedef pair<int,double> term;
+vector<term> terms[2];
+bool cmp(const term a,const term b)  {
+    return a.first>b.first;
+}
 int main()  {
-    string a;
-    a[0]='1';
-    a[1]='2';
-    cout<<a.c_str()<<endl;
+    int n,a;
+    double b;
+    cin>>n;
+    for (int i=0;i<n;i++)   {
+        cin>>a;
+        cin>>b;
+        terms[0].push_back(make_pair(a,b));
+    }
+    sort(terms[0].begin(),terms[0].end(),cmp);
+    for (auto d:terms[0])    {
+        cout<<d.first<<' '<<d.second<<endl;
+    }
     return 0;
 }
