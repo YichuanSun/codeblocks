40a879e0ab7ae2b6acdfdd4082fa3a7bea51a33e
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-08-18 19:10:03 +0800

    .

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,63 +1,23 @@
 #include <bits/stdc++.h>
-#define INF 0x3f3f3f3f
-#define V 505
 using namespace std;
-int dis[V][V],cost[V][V],d[V],pre[V],money[V];
-bool used[V];
+struct st{
+    char c;
+    int nex;
+};
+map<int,st> mmp;
+bool jud(map<int,st> a[i]) {
+    if (a[i].nex==-1)   return true;
+    else return false;
+}
 int main()  {
-    //ios::sync_with_stdio(false);
-    //cin.tie(0),cout.tie(0);
-    int n,m,s,D,c1,c2;
-    cin>>n>>m>>s>>D;
-    fill(d,d+V,INF);
-    fill(dis[0],dis[0]+V*V,INF);
-    fill(cost[0],cost[0]+V*V,INF);
-    fill(used,used+V,false);
-    fill(money,money+V,0);
-    d[s]=0;
-    for (int i=0;i<m;i++)   {
-        cin>>c1>>c2;
-        cin>>dis[c1][c2]>>cost[c1][c2];
-        dis[c2][c1]=dis[c1][c2];
-        cost[c2][c1]=cost[c1][c2];
+    st nst;
+    int be1,be2,n,f;
+    cin>>be1>>be2>>n;
+    for (int i=0;i<n;i++)   {
+        cin>>f;
+        cin>>nst.c>>nst.nex;
+        mmp[f]=nst;
     }
-    while (true)    {
-        int v=-1;
-        for (int i=0;i<n;i++)   {
-            if (!used[i]&&(v==-1||d[i]<d[v]))   v=i;
-        }
-        if (v==-1)  break;
-        used[v]=true;
-        for (int i=0;i<n;i++)   {
-            if (d[i]>d[v]+dis[v][i])   {
-                d[i]=d[v]+dis[v][i];
-                money[i]=money[v]+cost[v][i];
-                pre[i]=v;
-            }
-            else if (d[i]==d[v]+dis[v][i]) {
-                if (money[i]>money[v]+cost[v][i])   {
-                    money[i]=money[v]+cost[v][i];
-                    pre[i]=v;
-                }
-            }
-        }
-    }
-    stack<int> st;
-    st.push(D);
-    int k=D;
-    while (pre[k]!=s)   {
-        st.push(pre[k]);
-        k=pre[k];
-    }
-    st.push(s);
-    while (!st.empty()) {
-        cout<<st.top()<<' ';
-        st.pop();
-    }
-    cout<<d[D]<<' ';
-    cout<<money[D]<<endl;
+    std::map<int,st>::iterator it1=find_if(mmp.begin(),mmp.end(),jud);
     return 0;
 }
-
-
-
