13e11b446f7e2dbc49f50b8af7643aa4e8e52ef4
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-08-22 21:03:45 +0800

    ？

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,91 +1,26 @@
 #include <iostream>
-#include <cstring>
-#include <vector>
-#include <cstdlib>
-#include <cstdio>
 using namespace std;
-
-struct node{
-	int val;
-	node *left;
-	node *right;
-};
-
-vector<int>a;
-bool isBST=true;
-
-bool gt(const int &a,const int &b){
-	return a>=b;
-}
-
-bool lt(const int &a,const int &b){
-	return a<b;
-}
-
-node* build(int l,int r,bool cmp(const int&,const int&)){
-	if(!isBST) return NULL;
-	if(l>r) return NULL;
-	node *root=new node();
-	root->val=a[l];
-	if(l==r) return root;
-	int mid;
-	for(mid=l+1;mid<=r;mid++){
-		if(!cmp(a[mid],a[l])) {
-			break;
-		}
-	}
-	root->left=build(l+1,mid-1,cmp);
-	int j;
-	for(j=mid;j<=r;j++){
-		if(cmp(a[j],a[l])){
-			isBST=false;
-			return NULL;
-		}
-	}
-	root->right=build(mid,r,cmp);
-	return root;
-}
-
-bool flg=true;
-void postorder(node *root){
-	if(root){
-		postorder(root->left);
-		postorder(root->right);
-		if(flg){
-			cout<<root->val;
-			flg=false;
-		}
-		else{
-			cout<<" "<<root->val;
-		}
-	}
-}
 
 int main()
 {
-	int n;
-	cin>>n;
-	a.resize(n);
-	for(int i=0;i<n;i++){
-		cin>>a[i];
-	}
-	if(n==1){
-		cout<<"YES"<<endl;
-		cout<<a[0]<<endl;
-		return 0;
-	}
-	node *root=build(0,n-1,lt);
-	if(isBST){
-		cout<<"YES"<<endl;
-		postorder(root);
-	}
-	else{
-		isBST=true;
-		root=build(0,n-1,gt);
-		if(isBST){
-			cout<<"YES"<<endl;
-			postorder(root);
-		}
-		else cout<<"NO"<<endl;
-	}
+    int n,m,list[1001],loc[1001],p,q;
+    cin>>n>>m;
+    for (int i=1;i<=n;i++)
+        list[i]=loc[i]=i;
+    while (m--)
+    {
+        cin>>p>>q;
+        int d=q>0?1:-1;
+        for (int i=loc[p];i!=loc[p]+q;i+=d)
+        {
+            list[i]=list[i+d];
+            loc[list[i]]=i;
+        }
+        list[loc[p]+q]=p;
+        loc[p]+=q;
+    }
+    for (int i=1;i<n;i++)
+        cout<<list[i]<<" ";
+    cout<<list[n];
+    return 0;
 }
