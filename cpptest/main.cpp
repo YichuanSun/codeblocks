6ebcb5fcfda525426b67d4ea89f35413e923c359
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-08-23 16:33:12 +0800

    .

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,26 +1,17 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int n,m,list[1001],loc[1001],p,q;
-    cin>>n>>m;
-    for (int i=1;i<=n;i++)
-        list[i]=loc[i]=i;
-    while (m--)
-    {
-        cin>>p>>q;
-        int d=q>0?1:-1;
-        for (int i=loc[p];i!=loc[p]+q;i+=d)
-        {
-            list[i]=list[i+d];
-            loc[list[i]]=i;
-        }
-        list[loc[p]+q]=p;
-        loc[p]+=q;
+#include<cstdio>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a = 1,left,now,right,ans = 0;
+    while(n/a){
+        left = n / (a*10);
+        right = n % a;
+        now = n / a % 10;
+        if(now == 0) ans += left*a;
+        else if(now == 1) ans += left*a + right + 1;
+        else ans += (left+1)*a;
+        a *= 10;
     }
-    for (int i=1;i<n;i++)
-        cout<<list[i]<<" ";
-    cout<<list[n];
+    printf("%d",ans);
     return 0;
 }
