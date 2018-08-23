fb53605f0059409e0915fc8a96d8288e2b741976
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-08-23 20:56:27 +0800

    h

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,17 +1,34 @@
-#include<cstdio>
-int main(){
-    int n;
-    scanf("%d",&n);
-    int a = 1,left,now,right,ans = 0;
-    while(n/a){
-        left = n / (a*10);
-        right = n % a;
-        now = n / a % 10;
-        if(now == 0) ans += left*a;
-        else if(now == 1) ans += left*a + right + 1;
-        else ans += (left+1)*a;
-        a *= 10;
+#include <iostream>
+using namespace std;
+
+int sr[] = {3500, 3500+1500, 3500+4500, 3500+9000, 3500+35000, 3500+55000, 3500+80000 };
+int tr[] = {3, 10, 20, 25, 30, 35, 45};
+const int SIZE = sizeof(sr) / sizeof(int);
+
+int range[SIZE];
+
+int main()
+{
+    int t,s;
+    cin >> t;
+    if (t<=3500)    {
+        cout<<t<<endl;
+        return 0;
     }
-    printf("%d",ans);
+    range[0] = sr[0];
+    for(int i=1; i<SIZE; i++) {
+        range[i] = range[i-1] + (sr[i] - sr[i-1])
+                - (sr[i] - sr[i-1]) * tr[i-1] / 100;
+    }
+    int i;
+    for(i=0; i<SIZE; i++)
+        if(t <= range[i])
+            break;
+    if(i == 0)
+        s = t;
+    else {
+        s = sr[i-1] + (t - range[i-1]) * 100 / (100 - tr[i-1]);
+    }
+    cout << s << endl;
     return 0;
 }
