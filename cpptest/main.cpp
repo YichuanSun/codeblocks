7fa0233236e655d2e5958b798f435f766f8de9b3
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-08-27 14:08:40 +0800

    .

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -2,49 +2,57 @@
 using namespace std;
 struct node{
     int data;
-    node *l,*r;
+    node *r,*l;
 };
-int post[40],ino[40];
-bool flag;
-queue<node*> que;
-node* buildTree(int n,int post[],int ino[]);
-void bfs(node* root);
+stack<int> st;
+vector<int> v;
+int ino[50],pre[50];
+node* buildTree(int pre[],int ino[],int n);
+void postOrderTraversal(node* root);
 int main()  {
-    int n;
+    int n,t,cnt=0,ci=0;
+    string s;
     cin>>n;
-    for (int i=0;i<n;i++)   cin>>post[i];
-    for (int i=0;i<n;i++)   cin>>ino[i];
+    while (cnt<n||!st.empty()) {
+        cin>>s;
+        if (s[1]=='u')  {
+            cin>>t;
+            st.push(t);
+            pre[cnt++]=t;
+        }
+        else if (s[1]=='o') {
+            ino[ci++]=st.top();
+            st.pop();
+        }
+    }
+    for (int i=0;i<n;i++)   cout<<pre[i]<<' ';
+    cout<<endl;
+    for (int i=0;i<n;i++)   cout<<ino[i]<<' ';
+    cout<<endl;
     node* root=nullptr;
-    root=buildTree(n,post,ino);
-    bfs(root);
+    root=buildTree(pre,ino,n);
+    postOrderTraversal(root);
+    cout<<v[0];
+    for (int i=1;i<v.size();i++)    cout<<' '<<v[i];
     cout<<endl;
     return 0;
 }
 
-node* buildTree(int n,int post[],int ino[]) {
+node* buildTree(int pre[],int ino[],int n)  {
     if (n<=0)   return nullptr;
     int s=0;
-    while (ino[s]!=post[n-1])   s++;
+    while (ino[s]!=pre[0])  s++;
     node* newnode=new node;
     newnode->data=ino[s];
-    newnode->l=buildTree(s,post,ino);
-    newnode->r=buildTree(n-s-1,post+s,ino+s+1);
+    newnode->l=buildTree(pre+1,ino,s);
+    newnode->r=buildTree(pre+s+1,ino+s+1,n-s-1);
     return newnode;
 }
 
-void bfs(node* root)   {
-    que.push(root);
-    while (!que.empty())    {
-        node* tn=que.front();
-        if (!tn)    {
-            que.pop();
-            continue;
-        }
-        if (!flag)  cout<<tn->data;
-        else    cout<<' '<<tn->data;
-        flag=true;
-        que.push(tn->l);
-        que.push(tn->r);
-        que.pop();
-    }
+void postOrderTraversal(node* root)  {
+    if (!root)  return;
+    postOrderTraversal(root->l);
+    postOrderTraversal(root->r);
+    v.push_back(root->data);
 }
+
