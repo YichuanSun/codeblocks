0e7fc3b43bba41d9d8f93f50a2e33a040769914f
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-08-27 10:36:40 +0800

    h

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,90 +1,91 @@
-#include<iostream>
-#include<cstdio>
-#include<queue>
-#include<algorithm>
+#include <bits/stdc++.h>
+#define N 40
 using namespace std;
-struct node {
-    int v, height;
-    node *lchild, *rchild;
-}*root;
-node* newnode(int v) {
-    node* Node = new node;
-    Node->v = v;
-    Node->height = 1;
-    Node->lchild = Node->rchild = NULL;
-    return Node;
-}
-int getHeight(node* root) {
-    if(root == NULL) {
-        return 0;
+struct node{
+    int data,h;
+    node *left,*right;
+};
+int iar[N];
+int geth(node* p);
+node* inside(node* p,int t);
+node* rrot(node* p);
+node* lrot(node* p);
+node* rlrot(node* p);
+node* lrrot(node* p);
+int main()  {
+    int n,t;
+    cin>>n;
+    node *root=nullptr;
+    for (int i=0;i<n;i++){
+        cin>>t;
+        root=inside(root,t);
     }
-    return root->height;
-}
-void updatahegiht(node* root) {
-    root->height = max(getHeight(root->lchild), getHeight(root->rchild)) + 1;
-}
-int getbalancefac(node* root) {
-    return getHeight(root->lchild) - getHeight(root->rchild);
-}
-void L(node* &root) {
-    node* temp = root->rchild;
-    root->rchild = temp->lchild;
-    temp->lchild = root;
-    updatahegiht(root);
-    updatahegiht(temp);
-    root = temp;
-}
-void R(node* &root) {
-    node* temp = root->lchild;
-    root->lchild = temp->rchild;
-    temp->rchild = root;
-    updatahegiht(root);
-    updatahegiht(temp);
-    root = temp;
-}
-void insert(node* &root, int v) {
-    if(root == NULL) {
-        root = newnode(v);
-        return ;
-    }
-    if(v < root->v) {
-        insert(root->lchild, v);
-        updatahegiht(root);
-        if(getbalancefac(root) == 2) {
-            if(getbalancefac(root->lchild) == 1) {
-                R(root);
-            } else if(getbalancefac(root->lchild) == -1) {
-                L(root->lchild);
-                R(root);
-            }
-        }
-    } else {
-        insert(root->rchild, v);
-        updatahegiht(root);
-        if(getbalancefac(root) == -2) {
-            if(getbalancefac(root->rchild) == -1) {
-                L(root);
-            } else if(getbalancefac(root->rchild) == 1) {
-                R(root->rchild);
-                L(root);
-            }
-        }
-    }
-}
-node* create(int data[], int n) {
-    node* root = NULL;
-    for(int i = 0; i < n; i++) {
-        insert(root, data[i]);
-    }
-    return root;
-}
-int main() {
-    int n, v;
-    scanf("%d", &n);
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &v);
-        insert(root, v);
-    }
-    printf("%d\n", root->v);
+    cout<<root->data<<endl;
     return 0;
+}
+
+node* inside(node* p,int t) {
+    if (!p) {
+        p=new node;
+        p->data=t;
+        p->h=1;
+        p->left=p->right=nullptr;
+    }
+    else if (t>p->data) {
+        p->right=inside(p->right,t);
+        if (geth(p->left)-geth(p->right)==-2)  {
+            if (t>p->right->data)
+                p=rrot(p);
+            else
+                p=rlrot(p);
+        }
+    }
+    else if (t<p->data) {
+        p->left=inside(p->left,t);
+        if (geth(p->left)-geth(p->right)==2)    {
+            if (t<p->left->data)
+                p=lrot(p);
+            else
+                p=lrrot(p);
+        }
+    }
+    p->h=geth(p);
+    return p;
+}
+
+node* rrot(node* p) {
+    node* tp=p->right;
+    p->right=tp->left;
+    tp->left=p;
+    p->h=geth(p);
+    tp->h=max(geth(tp->right),tp->left->h)+1;
+    return tp;
+}
+
+node* lrot(node* p) {
+    node* tp=p->left;
+    p->left=tp->right;
+    tp->right=p;
+    p->h=geth(p);
+    tp->h=max(p->h,geth(tp->left))+1;
+    return tp;
+}
+
+node* lrrot(node* p)    {
+    p->left=rrot(p->left);
+    p=lrot(p);
+    return p;
+}
+
+node* rlrot(node* p)    {
+    p->right=lrot(p->right);
+    p=rrot(p);
+    return p;
+}
+
+int geth(node* p)   {
+    if (p==nullptr) return 0;
+    int lh=geth(p->left);
+    int rh=geth(p->right);
+    return max(lh,rh)+1;
 }
