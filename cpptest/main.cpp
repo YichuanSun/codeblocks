06eb5324b65562e9a09a8ae000b338a1dda037dd
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-08-27 11:45:03 +0800

    h

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,91 +1,42 @@
 #include <bits/stdc++.h>
-#define N 40
 using namespace std;
+int ino[50],pos[50];
 struct node{
-    int data,h;
+    int data;
     node *left,*right;
 };
-int iar[N];
-int geth(node* p);
-node* inside(node* p,int t);
-node* rrot(node* p);
-node* lrot(node* p);
-node* rlrot(node* p);
-node* lrrot(node* p);
-int main()  {
-    int n,t;
-    cin>>n;
-    node *root=nullptr;
-    for (int i=0;i<n;i++){
-        cin>>t;
-        root=inside(root,t);
+node* root;
+node* buildTree(int* i,int* p,int n);
+queue<node*> que;
+void bfs()  {
+    que.push(root);
+    while (!que.empty())    {
+        node* tnode;
+        tnode=que.front();que.pop();
+        if (tnode->left!=NULL)  que.push(tnode->left);
+        if (tnode->right!=NULL)  que.push(tnode->right);
+        cout<<tnode->data;
+        if (!que.empty())   cout<<' ';
+        else    cout<<endl;
     }
-    cout<<root->data<<endl;
+}
+int main()  {
+    int n;
+    cin>>n;
+    for (int i=0;i<n;i++)   cin>>pos[i];
+    for (int i=0;i<n;i++)   cin>>ino[i];
+    root=buildTree(ino,pos,n);
+    bfs();
     return 0;
 }
 
-node* inside(node* p,int t) {
-    if (!p) {
-        p=new node;
-        p->data=t;
-        p->h=1;
-        p->left=p->right=nullptr;
-    }
-    else if (t>p->data) {
-        p->right=inside(p->right,t);
-        if (geth(p->left)-geth(p->right)==-2)  {
-            if (t>p->right->data)
-                p=rrot(p);
-            else
-                p=rlrot(p);
-        }
-    }
-    else if (t<p->data) {
-        p->left=inside(p->left,t);
-        if (geth(p->left)-geth(p->right)==2)    {
-            if (t<p->left->data)
-                p=lrot(p);
-            else
-                p=lrrot(p);
-        }
-    }
-    p->h=geth(p);
-    return p;
-}
-
-node* rrot(node* p) {
-    node* tp=p->right;
-    p->right=tp->left;
-    tp->left=p;
-    p->h=geth(p);
-    tp->h=max(geth(tp->right),tp->left->h)+1;
-    return tp;
-}
-
-node* lrot(node* p) {
-    node* tp=p->left;
-    p->left=tp->right;
-    tp->right=p;
-    p->h=geth(p);
-    tp->h=max(p->h,geth(tp->left))+1;
-    return tp;
-}
-
-node* lrrot(node* p)    {
-    p->left=rrot(p->left);
-    p=lrot(p);
-    return p;
-}
-
-node* rlrot(node* p)    {
-    p->right=lrot(p->right);
-    p=rrot(p);
-    return p;
-}
-
-int geth(node* p)   {
-    if (p==nullptr) return 0;
-    int lh=geth(p->left);
-    int rh=geth(p->right);
-    return max(lh,rh)+1;
+node* buildTree(int* i,int* p,int n)   {
+    if (n<=0)   return nullptr; //决定了最后一步不是p,n-1;而是p+spot,n-spot-1
+    int spot=0;
+    while (i[spot]!=p[n-1])   spot++;
+    node* newnode=new node;
+    newnode->data=i[spot];
+    newnode->left=buildTree(i,p,spot);
+    newnode->right=buildTree(i+spot+1,p+spot,n-spot-1);
+    return newnode;
 }
