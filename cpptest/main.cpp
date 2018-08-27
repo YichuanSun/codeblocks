2534712f8c3190056cc4a67e5427aaa7f0422894
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-08-27 12:42:43 +0800

    a

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,42 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-int ino[50],pos[50];
 struct node{
     int data;
-    node *left,*right;
+    node *l,*r;
 };
-node* root;
-node* buildTree(int* i,int* p,int n);
+int post[40],ino[40];
+bool flag;
 queue<node*> que;
-void bfs()  {
-    que.push(root);
-    while (!que.empty())    {
-        node* tnode;
-        tnode=que.front();que.pop();
-        if (tnode->left!=NULL)  que.push(tnode->left);
-        if (tnode->right!=NULL)  que.push(tnode->right);
-        cout<<tnode->data;
-        if (!que.empty())   cout<<' ';
-        else    cout<<endl;
-    }
-}
+node* buildTree(int n,int post[],int ino[]);
+void bfs(node* root);
 int main()  {
     int n;
     cin>>n;
-    for (int i=0;i<n;i++)   cin>>pos[i];
+    for (int i=0;i<n;i++)   cin>>post[i];
     for (int i=0;i<n;i++)   cin>>ino[i];
-    root=buildTree(ino,pos,n);
-    bfs();
+    node* root=nullptr;
+    root=buildTree(n,post,ino);
+    bfs(root);
+    cout<<endl;
     return 0;
 }
 
-node* buildTree(int* i,int* p,int n)   {
-    if (n<=0)   return nullptr; //决定了最后一步不是p,n-1;而是p+spot,n-spot-1
-    int spot=0;
-    while (i[spot]!=p[n-1])   spot++;
+node* buildTree(int n,int post[],int ino[]) {
+    if (n<=0)   return nullptr;
+    int s=0;
+    while (ino[s]!=post[n-1])   s++;
     node* newnode=new node;
-    newnode->data=i[spot];
-    newnode->left=buildTree(i,p,spot);
-    newnode->right=buildTree(i+spot+1,p+spot,n-spot-1);
+    newnode->data=ino[s];
+    newnode->l=buildTree(s,post,ino);
+    newnode->r=buildTree(n-s-1,post+s,ino+s+1);
     return newnode;
+}
+
+void bfs(node* root)   {
+    que.push(root);
+    while (!que.empty())    {
+        node* tn=que.front();
+        if (!tn)    {
+            que.pop();
+            continue;
+        }
+        if (!flag)  cout<<tn->data;
+        else    cout<<' '<<tn->data;
+        flag=true;
+        que.push(tn->l);
+        que.push(tn->r);
+        que.pop();
+    }
 }
