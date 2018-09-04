6823be8607f54cfd4d01aa1bac8a8cf52a9253b1
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-04 21:25:23 +0800

    .

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,25 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int maxn = 1e5 + 10;
-int a[maxn], num[maxn];
-
-int main() {
-    int n;
-    scanf("%d", &n);
-    for(int i = 1; i <= n; i ++) {
-        scanf("%d", &a[i]);
-        if(a[i] < 0 || a[i] > n + 1) continue;
-        else
-            num[a[i]] ++;
-    }
-
-    for(int i = 1; i <= n; i ++) {
-        if(num[i] == 0) {
-           printf("%d\n", i);
-           return 0;
-        }
-    }
-    printf("%d\n", n + 1);
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    string s;
+    char a;
+    cin>>s;
+    cin>>a;
+    if (s.find(a)==s.npos)
+        cout<<"One Piece is true!"<<endl;
+    else    cout<<"Good Luck!"<<endl;
     return 0;
 }
