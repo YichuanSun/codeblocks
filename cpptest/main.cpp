cf71469dd1fe5fc0c2886113ddaf7efd157ba1c0
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-04 20:55:20 +0800

    .

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,13 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-ll gcd(ll x,ll y)    {
-    if (x%y==0) return y;
-    return gcd(y,x%y);
-}
-int main()  {
-    int a,b;
-    cin>>a>>b;
-    cout<<gcd(a,b)<<endl;
+
+const int maxn = 1e5 + 10;
+int a[maxn], num[maxn];
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i ++) {
+        scanf("%d", &a[i]);
+        if(a[i] < 0 || a[i] > n + 1) continue;
+        else
+            num[a[i]] ++;
+    }
+
+    for(int i = 1; i <= n; i ++) {
+        if(num[i] == 0) {
+           printf("%d\n", i);
+           return 0;
+        }
+    }
+    printf("%d\n", n + 1);
     return 0;
 }
