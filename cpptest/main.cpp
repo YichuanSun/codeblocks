e83ed5ffb1b923c522cfb6f88d25a4d5ec1bb299
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-16 10:45:29 +0800

    ？

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()  {
+int main(){
     int n;
-    int a[]={23,31,45,67,89,91,96};
-    int *b=upper_bound(a,a+7,89);
-    int *c=lower_bound(a,a+7,89);
-    cout<<b-c<<endl;
+    while (scanf("%d",&n)!=EOF)   {
+        cout<<"YES"<<endl;
+    }
     return 0;
 }
