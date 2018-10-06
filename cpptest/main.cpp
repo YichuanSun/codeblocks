b1baac5af5eb22cb461902d657eb95e5d44ba38f
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-07 01:32:51 +0800

    啊？

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,20 +1,25 @@
 #include <bits/stdc++.h>
-#define N 30
+#define N 2005
+#define INF 0x7fffffff
 using namespace std;
-char s[N][N];
+typedef long long ll;
+int w[N],v[N],dp[N];
 int main()  {
-    int n,m;
-    cin>>n>>m;
-    for (int i=0;i<n;i++)   {
-        for (int j=0;j<m;j++)   {
-            do{s[i][j]=getc(stdin);}
-            while (s[i][j]=='\n');
-        }
-    }
-    for (int i=0;i<n;i++){
-        for (int j=0;j<m;j++)
-            cout<<s[i][j]<<' ';
-        cout<<endl;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n,W,cnt=0;
+    fill(dp,dp+N,0);
+    cin>>n>>W;
+    for (int i=0;i<n;i++)  cin>>w[i];
+    for (int i=0;i<n;i++)  cin>>v[i];
+    for (int i=0;i<n;i++)
+        for (int j=w[i];j<=W;j++)   {
+            dp[j]=max(dp[j],dp[j-w[i]]+v[i]);
+            cout<<"Case "<<++cnt<<endl;
+            for (int i=0;i<=W;i++)   cout<<i<<' ';
+            cout<<endl;
+            for (int i=0;i<=W;i++)   cout<<dp[i]<<' ';
+            cout<<endl;
     }
     return 0;
 }
