abf615310fd2e0b47b5ed14fc55eb510613c83ca
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-08 20:57:27 +0800

    嗯？Excuse me?

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,25 +1,21 @@
 #include <bits/stdc++.h>
-#define N 2005
-#define INF 0x7fffffff
 using namespace std;
-typedef long long ll;
-int w[N],v[N],dp[N];
+const int maxn=100005;
+int sTree[maxn<<2],A[maxn],n;
 int main()  {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n,W,cnt=0;
-    fill(dp,dp+N,0);
-    cin>>n>>W;
-    for (int i=0;i<n;i++)  cin>>w[i];
-    for (int i=0;i<n;i++)  cin>>v[i];
-    for (int i=0;i<n;i++)
-        for (int j=w[i];j<=W;j++)   {
-            dp[j]=max(dp[j],dp[j-w[i]]+v[i]);
-            cout<<"Case "<<++cnt<<endl;
-            for (int i=0;i<=W;i++)   cout<<i<<' ';
-            cout<<endl;
-            for (int i=0;i<=W;i++)   cout<<dp[i]<<' ';
-            cout<<endl;
-    }
     return 0;
+}
+//更新单个节点的值，此处为求和
+void PushUp(int rt) {sTree[rt]=sTree[rt<<1]+sTree[rt<<1|1];}
+//build函数建树
+void Build(int l,int r,int rt)  {
+    if (l==r)   {sTree[rt]=A[l];return;}
+    int m=(l+r)>>1;
+    Build(l,m,rt<<1);
+    Build(m+1,r,rt<<1|1);
+    PushUp(rt);
+}
+
+void update(int l,int r,int rt) {
+    if ()
 }
