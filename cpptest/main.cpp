56f1724f51603e7ec62166f1cd4fb91d9d8ae20c
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-10 23:28:45 +0800

    ...今天太忙了，空余时间都在写那个电工实验预习，真难

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,25 +1,18 @@
 #include <bits/stdc++.h>
+#define N 1005
 using namespace std;
-const int maxn=100005;
-int sTree[maxn<<2],A[maxn],n;
+int a[N],b[N];
 int main()  {
+    int n;
+    cin>>n;
+    for (int i=1;i<=n;i++)
+    {
+        cin>>a[i];
+        b[i]=b[i-1]+a[i];
+    }
+    for (int i=0;i<=n;i++)  cout<<a[i]<<' ';
+    cout<<endl;
+    for (int i=0;i<=n;i++)  cout<<b[i]<<' ';
+    cout<<endl;
     return 0;
-}
-//更新单个节点的值，此处为求和
-void PushUp(int rt) {sTree[rt]=sTree[rt<<1]+sTree[rt<<1|1];}
-//build函数建树
-void Build(int l,int r,int rt)  {
-    if (l==r)   {sTree[rt]=A[l];return;}
-    int m=(l+r)>>1;
-    Build(l,m,rt<<1);
-    Build(m+1,r,rt<<1|1);
-    PushUp(rt);
-}
-//修改A[L]+=C单个值，对线段树的修改
-void update(int L,int C,int l,int r,int rt) {
-    if (l==r)   {sTree[rt]+=C;return;}
-    int m=(l+r)>>1;
-    if (L<=m)   update(L,C,l,m,rt<<1);
-    else    update(L,C,m+1,r,rt<<1|1);
-    PushUp(rt);
 }
