26aad65c7ef4565bb8ff74584af6ae6a4c8e3e96
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-14 09:43:12 +0800

    一个小实验，试了试string初始化内存空间
    可以用string s(int,char)来初始化一个string，比如吧char设成\0就很好。

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,18 +1,11 @@
 #include <bits/stdc++.h>
 #define N 1005
 using namespace std;
-int a[N],b[N];
 int main()  {
-    int n;
-    cin>>n;
-    for (int i=1;i<=n;i++)
-    {
-        cin>>a[i];
-        b[i]=b[i-1]+a[i];
-    }
-    for (int i=0;i<=n;i++)  cout<<a[i]<<' ';
-    cout<<endl;
-    for (int i=0;i<=n;i++)  cout<<b[i]<<' ';
-    cout<<endl;
+    char c;
+    string s(10,'\0');
+    cin>>c;
+    s[0]=c;
+    cout<<s<<endl;
     return 0;
 }
