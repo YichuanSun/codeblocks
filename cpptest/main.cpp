9afed3d0ed565370a56b7851f6925006b9b5d198
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-08-17 21:32:37 +0800

    。

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,9 +1,40 @@
 #include <bits/stdc++.h>
-#define N 15
+#define N 1005
 using namespace std;
-
+int re(int a[],int b[],int n)   {
+    int sz=0;
+    if (n==1)   {b[sz++]=a[0];b[sz++]=1;return sz;}
+    int c=a[0],cnt=1;
+    for (int i=1;i<n;i++)   {
+        if (a[i]==c)    cnt++;
+        else {
+            b[sz++]=c;
+            stack<int> sa;
+            while (cnt) {
+                sa.push(cnt%10);
+                cnt/=10;
+            }
+            while (!sa.empty()){b[sz++]=sa.top();sa.pop();}
+            cnt=1;
+            c=a[i];
+        }
+    }
+    b[sz++]=c;
+    stack<int> sa;
+    while (cnt) {
+        sa.push(cnt%10);
+        cnt/=10;
+    }
+    while (!sa.empty()){b[sz++]=sa.top();sa.pop();}
+    return sz;
+}
+int a[N],b[N];
 int main()  {
-    int a[N]={0};
-    for (int i=0;i<N;i++)   printf("%d\n",a[i]);
+    int n;
+    cin>>n;
+    for (int i=0;i<n;i++)   cin>>a[i];
+    int sz=re(a,b,n);
+    for (int i=0;i<sz;i++)  cout<<b[i];
+    cout<<endl;
     return 0;
 }
